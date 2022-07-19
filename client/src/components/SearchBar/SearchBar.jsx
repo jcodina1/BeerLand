7f1ca9f5bbf3d0fa94bee5039f3e4d5cd68e3c81
Juan Pablo <ballesteros.jp.rj45@gmail.com>
2022-07-19 18:40:30 -0300

    SearchBarIntento2

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,51 @@
-import React from "react";
+import { React, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { searchBar, getAllBeers} from '../../redux/actions/index';
 
-export default function SearchBar() {
-  return <div>SearchBar</div>;
+//function validation(payload){
+//     let error = {};
+    
+//     if(!payload || isNaN(payload) !== true){
+//         error.payload = 'thats not a dogito!'                             
+//     }
+//     return error;
+// };
+
+export default function SearchBar({setPage}){
+const dispatch = useDispatch();
+const [ payload, setSearch ] = useState('');
+// const [ error, setError ] = useState({});
+
+function handleInput(e){
+    e.preventDefault();
+    setSearch(e.target.value);
+    // setError(validation(payload))
+}
+
+function handleSubmit(e){
+    e.preventDefault();
+    // setError(validation(payload));
+    // const error = validation(payload);
+
+    // if(Object.values(error).length === 0){
+        dispatch(searchBar(payload));
+        //setPage(1)
+        setSearch('');
+    //}else{
+    //     alert('thats not a dog!')
+    // }  
+}
+
+function handleBack(){
+    dispatch(getAllBeers());
+    //setPage(1);
+}
+
+    return (
+        <div >
+            <input  value={payload} type='text' placeholder='Search a beer...' onChange={(e) => handleInput(e)}/>
+            <button type='submit' onClick={(e)=> handleSubmit(e)}>Search</button>
+            <button onClick={handleBack}>Clean search</button>
+        </div>
+    )
 }
--- client/src/redux/actions/index.jsx
+++ client/src/redux/actions/index.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
-import ALL_API from "../const";
+import {ALL_API, ALL_ID, ALL_NAME, GET_BEERS, GET_BEER_DETAIL, REMOVE_DETAIL, SEARCH_BAR } from "../const";
 
 export function getAllBeers() {
   return async function (dispatch) {
-    var allBeers = await axios.get("https://beerland.herokuapp.com/beer");
+    var allBeers = await axios.get(ALL_API);
     return dispatch({
-      type: "GET_BEERS",
+      type: GET_BEERS,
       payload: allBeers.data,
     });
   };
@@ -13,18 +13,32 @@ export function getAllBeers() {
 
 export function getBeerDetail(id) {
   return async function (dispatch) {
-    const beerById = await axios.get(
-      `https://beerland.herokuapp.com/beer/id/${id}`
-    );
+    const beerById = await axios.get(ALL_ID + id);
     return dispatch({
-      type: "GET_BEER_DETAIL",
+      type: GET_BEER_DETAIL,
       payload: beerById.data,
     });
   };
-}
+};
 
 export const removeDetail = () => {
   return {
-    type: "REMOVE_DETAIL",
+    type: REMOVE_DETAIL,
   };
+};
+
+export function searchBar(payload){
+  return async function(dispatch){
+      try{
+          const search = await axios.get(ALL_NAME + payload);
+          return dispatch ({
+              type: SEARCH_BAR,
+              payload: search.data
+          })
+      } catch(error){
+          if(error.response){
+              alert(error.response.data)
+          }
+      }
+  }
 };
