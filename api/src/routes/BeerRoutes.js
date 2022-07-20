5f71e5659099e3eae765a318397eb023b3da15e6
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-07-20 10:04:47 -0500

    Pinche seller

--- api/src/routes/BeerRoutes.js
+++ api/src/routes/BeerRoutes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-const { getAllBeers, getBeerID, updateBeer, deleteBeer } = require('../controllers/BeerController');
+const { getAllBeers, getBeerID, updateBeer, deleteBeer, postBeer } = require('../controllers/BeerController');
 
 const router = Router();
 
 router.get('/id/:id', getBeerID)
 router.get('/', getAllBeers)
-router.post('/')
+router.post('/',postBeer)
 router.put('/update/:id', updateBeer)
 router.delete('/', deleteBeer)
 
