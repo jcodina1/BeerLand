c81a2c57a85f2048d63251648116ed6d3d183d6f
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-07-21 18:37:24 -0500

    add form

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import BeerDetail from "./components/BeerDetail/BeerDetail";
 import Login from "./components/Login/login";
 import Register from "./components/register/resgister";
 import { AuthProvider } from "./components/context/authContext";
+import AddProduct from "./components/AddProduct/AddProduct";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
            <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/home" component={Home} />
+          <Route exact path="/add" component={AddProduct} />
           <Route path="/beers/detail/:id" component={BeerDetail} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
