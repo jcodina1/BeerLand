6cbf78f09a9c57151f0de6b6bad908228fe8ba96
tomitomitomitomitomitomi <tomasmaqui@gmail.com>
2022-07-27 20:43:20 -0400

    paypal-react test

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,10 +14,9 @@ const firebaseConfig = {
   storageBucket: "project-a9a3c.appspot.com",
   messagingSenderId: "777692993210",
   appId: "1:777692993210:web:7ea37cf97e045bf126d483",
-  measurementId: "G-PJCY7BSSG3"
+  measurementId: "G-PJCY7BSSG3",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
