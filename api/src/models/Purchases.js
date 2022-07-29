df62501a41de21d8b76ff00dc2f0a6501db1fcf4
tomitomitomitomitomitomi <tomasmaqui@gmail.com>
2022-07-29 15:40:10 -0400

    modelo de compra

--- api/src/models/Purchases.js
+++ api/src/models/Purchases.js
@@ -3,11 +3,40 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("purchases", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    }
-  }, { timestamps: false });
+  sequelize.define(
+    "purchases",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+      },
+      sellerId: {
+        type: DataTypes.INTEGER,
+      },
+      purchaseDetail: {
+        type: DataTypes.JSONB,
+      },
+      totalValue: {
+        type: DataTypes.INTEGER,
+      },
+      paymentMethod: {
+        type: DataTypes.STRING,
+      },
+      delivery: {
+        type: DataTypes.BOOLEAN,
+      },
+    },
+
+    { paranoid: true }
+  );
 };
+
+//      beerQuantityExample = {
+//             'id1' : [cantidad,precio]
+//                   ...
+//             'idN' : [cantidad,precio]
+//               }
--- api/src/routes/UserRoutes.js
+++ api/src/routes/UserRoutes.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 
-<<<<<<< HEAD
+const { routes } = require("../app");
 const {
   getAllUsers,
+  Favorites,
+  getUserFav,
   postUser,
   postFavorite,
   getUserId,
   deleteFavorite,
-  Favorites,
 } = require("../controllers/UserController");
 
 const router = Router();
@@ -18,21 +19,6 @@ router.post("/fav", postFavorite);
 router.get("/id/:id", getUserId);
 router.delete("/fav", deleteFavorite);
 router.get("/fav/beer", Favorites);
-=======
-const { routes } = require('../app');
-const { getAllUsers, Favorites,getUserFav,postUser, postFavorite, getUserId, deleteFavorite } = require('../controllers/UserController');
-
-
-
-const router = Router();
-
-router.get('/', getAllUsers)
-router.post('/', postUser)
-router.post('/fav', postFavorite)
-router.get('/id/:id',getUserId)
-router.delete('/fav',deleteFavorite)
-router.get('/fav/beer',Favorites)
-router.get('/fav/beer/:id',getUserFav)
->>>>>>> origin/develop
+router.get("/fav/beer/:id", getUserFav);
 
 module.exports = router;
