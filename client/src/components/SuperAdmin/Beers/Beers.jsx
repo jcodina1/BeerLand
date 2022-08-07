7280105e9d3a62dab0b049deb5fb3cb3095ea23e
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-08-07 02:21:34 -0500

    DashBoard

---
+++ client/src/components/SuperAdmin/Beers/Beers.jsx
@@ -0,0 +1,20 @@
+import logo2 from '../../../img/BeerLogo2.png'
+import logo3 from '../../../img/BeerLogo3.png'
+import profiles from '../../../img/beertest.png'
+import { AiOutlineMenu } from "react-icons/ai";
+import { BsCart4, BsGrid1X2 } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
+import { MdOutlineAdminPanelSettings } from "react-icons/md";
+import { BiLogOut, BiPurchaseTagAlt, BiSearchAlt } from 'react-icons/bi'
+
+import style from './Beers.module.css'
+import { useState } from 'react';
+
+export default function Beers() {
+
+    return (
+        <div className={style.Beers}>            
+            Beers
+        </div>
+    )
+}
--- client/src/components/SuperAdmin/SideBar/Sidebar.jsx
+++ client/src/components/SuperAdmin/SideBar/Sidebar.jsx
@@ -6,72 +6,71 @@ import { BsCart4, BsGrid1X2 } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
 import { MdOutlineAdminPanelSettings } from "react-icons/md";
 import { BiLogOut, BiPurchaseTagAlt, BiSearchAlt } from 'react-icons/bi'
-import { Link } from 'react-router-dom';
-import style from './Sidebar.module.css'
 
-export default function Sidebar() {
+import style from './Sidebar.module.css'
+import { useState } from 'react';
+
+export default function Sidebar({mostrar,setMostrar,setRender}) {
 
     return (
-        <div className={style.sideBar}>
+        <div className={mostrar?style.sideBar:style.sideBar2}>            
+            <div className={style.menuBtn}> <AiOutlineMenu onClick={()=>mostrar?setMostrar(false):setMostrar(true)} /></div>
             <div className={style.logo_content}>
                 <div className={style.logo}>
-                    <img className={style.sombreo} src={logo2} alt="logo" />
-                    <img className={style.logo_name} src={logo3} alt="logo" />
+                    <img className={style.sombrero} src={logo2} alt="logo" />
+                    {mostrar?<img className={style.logo_name} src={logo3} alt="logo" />:''}
                 </div>
             </div>
-            <div className={style.menuBtn}> <AiOutlineMenu /></div>
             <ul className={style.nav_list}>
+                
                 <li>
-                    <div className={style.search}>
-                    <div className={style.links_icon}><BiSearchAlt className={style.links_icon_lupa} /> </div>
-                    <input className={style.links_name} placeholder='Search...' />
-                    </div>
-                </li>
-                <li>
-                    <Link to='/test' className={style.link}>
+                    <div onClick={()=>setRender("DashBoard")} className={style.link} >
                         <div className={style.links_icon}><BsGrid1X2 /> </div>
-                        <span className={style.links_name}>DashBoard</span>
-                    </Link>
-
+                        {mostrar?<span className={style.links_name}>DashBoard</span>:<span className={style.tooltip}>DashBoard</span>}
+                    </div>
+                    
                 </li>
                 <li>
-                    <Link to='/test' className={style.link}>
+                    <div onClick={()=>setRender("Users")} className={style.link}>
                         <div className={style.links_icon}><FaUserCircle /> </div>
-                        <span className={style.links_name}>Users</span>
-                    </Link>
-
+                        {mostrar?<span className={style.links_name}>Users</span>:<span className={style.tooltip}>Users</span>}
+                    </div>
+                    
                 </li>
                 <li>
-                    <Link to='/test' className={style.link}>
+                    <div onClick={()=>setRender("Sellers")} className={style.link}>
                         <div className={style.links_icon}><MdOutlineAdminPanelSettings /> </div>
-                        <span className={style.links_name}>Sellers</span>
-                    </Link>
+                        {mostrar?<span className={style.links_name}>Sellers</span>:<span className={style.tooltip}>Sellers</span>}
+                    </div>
+                    
                 </li>
                 <li>
-                    <Link to='/test' className={style.link}>
+                    <div onClick={()=>setRender("Products")} className={style.link}>
                         <div className={style.links_icon}><BsCart4 /></div>
-                        <span className={style.links_name}>Products</span>
-                    </Link>
+                        {mostrar?<span className={style.links_name}>Products</span>:<span className={style.tooltip}>Products</span>}
+                    </div>
+                    
                 </li>
                 <li>
-                    <Link to='/test' className={style.link}>
+                    <div onClick={()=>setRender("Pusrchases")} className={style.link}>
                         <div className={style.links_icon}><BiPurchaseTagAlt /> </div>
-                        <span className={style.links_name}>Pusrchases</span>
-                    </Link>
+                        {mostrar?<span className={style.links_name}>Pusrchases</span>:<span className={style.tooltip}>Pusrchases</span>}
+                    </div>
+                    
                 </li>
             </ul>
-            <div className={style.profile_content}>
+            {mostrar?<div className={style.profile_content}>
                 <div className={style.profile}>
                     <div className={style.profile_details}>
-                    <img src={profiles} alt="" />
+                        <img src={profiles} alt="" />
                         <div className={style.name_job}>
                             <div className={style.name}>JuanCho</div>
                             <div className={style.job}>Fullstack</div>
                         </div>
                     </div>
-                    <div className={style.log_out}><BiLogOut/></div>
+                    <div className={style.log_out}><BiLogOut /></div>
                 </div>
-            </div>
+            </div>:''}
         </div>
     )
 }
