4cd5ec9664066c08fbbf8b672aba78180bb94533
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-08-07 20:03:50 -0500

    Tabla User

--- client/src/components/SuperAdmin/Users/Users.jsx
+++ client/src/components/SuperAdmin/Users/Users.jsx
@@ -1,11 +1,82 @@
-import React from "react"
+import { useState } from "react";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import styles from "./Users.module.css";
+import { useSelector } from "react-redux";
 
 
 
 export default function Users(){
-return(
-  <div>hola
+  const TAX_RATE = 0.05;
+  const purchases=useSelector(state=>state.allPurchases)
+  // const invoiceSubtotal = purchase.totalPrice;
+  // const invoiceTaxes = TAX_RATE * purchase.totalPrice;
+  // const invoiceTotal = purchase.totalPrice - (TAX_RATE * purchase.totalPrice);
+  const [mostrar, setMostrar] = useState(false)
 
-  </div>
-)
+  function ccyFormat(num) {
+    return `${num.toFixed(2)}`;
+  }
+
+  return (
+    <div className={styles.table}>
+      {mostrar === true ?
+        purchases.map(purchase=>
+          <div>
+          <h3>Status: {purchase.status} </h3>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 600, maxWidth: 1000 }} aria-label="spanning table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" colSpan={3}>
+                    Details
+                  </TableCell>
+                  <TableCell align="right">Price</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Desc</TableCell>
+                  <TableCell align="right">Qty.</TableCell>
+                  <TableCell align="right">Unit</TableCell>
+                  <TableCell align="right">Sum</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {purchase.beers.map((beer, index) => (
+                  <TableRow key={beer.name}>
+                    <TableCell>{beer.name}</TableCell>
+                    <TableCell align="right">{purchase.purchaseDetails.filter(f => f.beerId === beer.id)[0].cant}</TableCell>
+                    <TableCell align="right">{beer.price}</TableCell>
+                    <TableCell align="right">{purchase.purchaseDetails.filter(f => f.beerId === beer.id)[0].cant * beer.price}</TableCell>
+                  </TableRow>
+                ))}
+                <TableRow>
+                  <TableCell rowSpan={3} />
+                  <TableCell colSpan={2}>Subtotal</TableCell>
+                  <TableCell align="right">{purchase.totalPrice}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Tax</TableCell>
+                  <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
+                  <TableCell align="right">{ccyFormat( TAX_RATE * purchase.totalPrice)}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={2}>TOTAL</TableCell>
+                  <TableCell align="right">{ccyFormat(purchase.totalPrice - (TAX_RATE * purchase.totalPrice))}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Button variant="contained" color="warning" onClick={() => setMostrar(false)}>Close</Button>
+        </div>)
+        : <Button variant="contained" color="warning" onClick={() => setMostrar(true)}>Details</Button>
+      }
+    </div>
+  );
 }
