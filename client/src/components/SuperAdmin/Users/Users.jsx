5316776544a7a12e4fa72911d9fae37de6a84ee3
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-08-07 19:37:09 -0500

    Archivos DashBoard

--- client/src/components/SuperAdmin/Users/Users.jsx
+++ client/src/components/SuperAdmin/Users/Users.jsx
@@ -1,20 +1,11 @@
-import logo2 from '../../../img/BeerLogo2.png'
-import logo3 from '../../../img/BeerLogo3.png'
-import profiles from '../../../img/beertest.png'
-import { AiOutlineMenu } from "react-icons/ai";
-import { BsCart4, BsGrid1X2 } from "react-icons/bs";
-import { FaUserCircle } from "react-icons/fa";
-import { MdOutlineAdminPanelSettings } from "react-icons/md";
-import { BiLogOut, BiPurchaseTagAlt, BiSearchAlt } from 'react-icons/bi'
+import React from "react"
 
-import style from './Users.module.css'
-import { useState } from 'react';
 
-export default function Users() {
 
-    return (
-        <div className={style.sideBar}>            
-            Usuarios
-        </div>
-    )
+export default function Users(){
+return(
+  <div>hola
+
+  </div>
+)
 }
