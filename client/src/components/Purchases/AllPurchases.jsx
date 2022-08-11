5f9c08b4411fcd2b0bed4eff4a21d812aeec3259
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-08-10 20:09:00 -0500

    AllPurchases

--- client/src/components/Purchases/AllPurchases.jsx
+++ client/src/components/Purchases/AllPurchases.jsx
@@ -3,7 +3,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getAllPurchases } from '../../redux/actions';
 import Purchase from '../Purchases/Purchase.jsx';
 import AllFilterStatus from './FilterStatus/AllFilterStatus'
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import style from "./Purchases.module.css"
 export default function Purchases() {
     const dispatch = useDispatch();
     const purchases = useSelector((state) => state.filtroPurchases)
@@ -17,6 +24,25 @@ export default function Purchases() {
     return (
         <div>
             <AllFilterStatus/>
+            <div className={style.table}>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Id purchase</TableCell>
+                            <TableCell>Status purchase</TableCell>
+                            <TableCell>Date</TableCell>
+                            <TableCell>Id User</TableCell>
+                            <TableCell>User Name</TableCell>
+                            <TableCell>User Email</TableCell>
+                            <TableCell>Address</TableCell>
+                            <TableCell>Total</TableCell>
+                            <TableCell>Detail</TableCell>
+                            
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        
             {purchases?.map(e =><Purchase
                         key={e.id}
                         id={e.id}
@@ -28,6 +54,10 @@ export default function Purchases() {
                         seller={e.beers.seller}
                         address={e.address}
                     /> )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
         </div>
     )
 }
