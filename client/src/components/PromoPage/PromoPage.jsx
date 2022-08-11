9d5a770471cde3e7722f3862b07bdeb93bfb070b
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-08-11 07:41:50 -0500

    Update PromoPage.jsx

--- client/src/components/PromoPage/PromoPage.jsx
+++ client/src/components/PromoPage/PromoPage.jsx
@@ -34,7 +34,7 @@ export default function PromoPage() {
 
                  <div >
                     <div className={style.dataUp}>
-                        <h1>We add Paypal and Cyrpto so you can buy your book!</h1>
+                        <h1>We add Paypal and Cyrpto so you can buy your beer!</h1>
                         <img src="https://cdn.blogtienao.com/wp-content/uploads/2021/11/bitcoin-xu-ly-nhieu-hon-paypal-62-tong-gia-tri-giao-dich-trong-nam-2021.jpg" alt="" />
 
                     </div>
