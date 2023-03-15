7b74da61c4fc258bf885b3e166914ef68353ab36
Juan Codina <codina844@gmail.com>
2023-03-15 14:17:51 -0500

    Deploy railway

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { getAllSellers2 } = require("./src/controllers/SellerController");
+const port =process.env.PORT||3001
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(port, () => {
     getAllSellers2();
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
